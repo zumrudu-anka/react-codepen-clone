7be5495e54aaa5d9df0b68f38a3097296e353312
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2021-02-01 00:25:35 +0300

    Updates

---
+++ precache-manifest.0af5fdbf17f5b8d0face6d2d7ee2aff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f27bf0dfb343e0676aabbd7851dd40",
+    "url": "/react-codepen-clone/index.html"
+  },
+  {
+    "revision": "a50a2ad68d9669a0bbfb",
+    "url": "/react-codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "70c4c6b12ea04f7aa170",
+    "url": "/react-codepen-clone/static/css/main.ad72a8a2.chunk.css"
+  },
+  {
+    "revision": "a50a2ad68d9669a0bbfb",
+    "url": "/react-codepen-clone/static/js/2.90752ba3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-codepen-clone/static/js/2.90752ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c4c6b12ea04f7aa170",
+    "url": "/react-codepen-clone/static/js/main.0c7547fd.chunk.js"
+  },
+  {
+    "revision": "25f2e3af20f29d7a9a92",
+    "url": "/react-codepen-clone/static/js/runtime-main.33d9aa38.js"
+  }
+]);
